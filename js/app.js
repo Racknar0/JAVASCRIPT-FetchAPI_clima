f87391349bd2a27dd99cf26dbac8e6ec8d6171d1
Racknar0 <racknarow1@gmail.com>
2022-02-27 17:32:30 -0500

    Add Selectors

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima)
+})
+
+function buscarClima (e) {
+    e.preventDefault();
+
+
+    //! Validar FORMULARIO
+    
+
+    //!colusltar api
+}
