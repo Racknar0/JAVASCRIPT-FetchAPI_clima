8b3586b6616be7b152543095253c9871de947961
Racknar0 <racknarow1@gmail.com>
2022-02-27 17:49:26 -0500

    Add Validation

--- js/app.js
+++ js/app.js
@@ -12,7 +12,37 @@ function buscarClima (e) {
 
 
     //! Validar FORMULARIO
-    
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
 
-    //!colusltar api
+
+    if ( ciudad === '' ||  pais === '') {
+        // Hubo un error
+        mosrtarError('Ambos Campos son Obligatorios');
+        return;
+    }
+
+    //!consultar api
+
+
+}
+
+function mosrtarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100')
+
+    if (!alerta) {
+
+        //Crear alerta
+        const alerta = document.createElement('DIV');
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4,', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center', 'fallo');
+        alerta.innerHTML = `
+        <strong class="font-bold">Error!</strong>
+        <span class="block">${mensaje}</span>`
+        container.appendChild(alerta);
+
+        //! ELIMINAR ALERTA
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
 }
